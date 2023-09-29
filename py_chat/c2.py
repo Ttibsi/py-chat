14a6da32ff5b259dc1c4920dbab631f8ff99c614
Ttibsi <ashisbitt@icloud.com>
2023-09-29 09:42:46 +0100

    temp

--- py_chat/c2.py
+++ py_chat/c2.py
@@ -1,11 +1,15 @@
 import os
+import random
 import socket
 import threading
 
+from textual import log
+from textual import work
 from textual.app import App
 from textual.app import ComposeResult
 from textual.containers import Container
 from textual.containers import VerticalScroll
+from textual.reactive import reactive
 from textual.widgets import Button
 from textual.widgets import Header
 from textual.widgets import Input
@@ -13,20 +17,11 @@ from textual.widgets import Label
 from textual.widgets import Static
 
 
-def client_recv(s: socket.socket, m: list[str]) -> None:
-    while True:
-        try:
-            m.append(s.recv(1024).decode("utf-8"))
-        except Exception as e:
-            print(f"ERROR: {e}")
-            s.close()
-            break
-
-
 class Window(App):
-    messages: list[str] = []
+    messages: reactive[tuple[str]] = reactive(())
     connected_users: list[str] = []
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    send_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    recv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     user = ""
 
     def compose(self) -> ComposeResult:
@@ -46,33 +41,43 @@ class Window(App):
     def on_mount(self) -> None:
         # Connect to sockets here
         # TODO: Username input
-        if os.name == "posix":
-            self.user = os.environ["USER"]
-        elif os.name == "nt":  # windows
-            self.user = os.environ["USERDOMAIN"]
-        else:
-            return
+        # if os.name == "posix":
+        #     self.user = os.environ["USER"]
+        # elif os.name == "nt":  # windows
+        #     self.user = os.environ["USERDOMAIN"]
+        # else:
+        #     return
+
+        self.user = str(random.randint(0, 10))
+        self.title = "Py-Chat Client"
 
         # TODO: Replace gethostname with a server ip -- perhaps entered by the
         # user at startup?
-        self.sock.connect((socket.gethostname(), 6789))
-        self.sock.send(self.user.encode("utf-8"))
+        self.send_sock.connect((socket.gethostname(), 6789))
+        self.send_sock.send(self.user.encode("utf-8"))
+        self.client_recv()
 
-        recv_thread = threading.Thread(
-            target=client_recv, args=(self.sock, self.messages)
-        )
-        recv_thread.start()
+    @work(exclusive=True, thread=True)
+    def client_recv(self):
+        log("STARTING SUB THREAD")
+        while True:
+            try:
+                self.messages += self.recv_sock.recv(1024).decode("utf-8")
+                log("TOTAL MESSAGES: " + self.messages)
+            except Exception as e:
+                print(f"ERROR: {e}")
+                self.recv_sock.close()
+                break
+
+            view = self.query_one("#history", Static)
+            view.update("\n".join(self.messages))
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         # only button currently is "submit"
+        log("BUTTON PRESS")
         msg = self.query_one("#input").value
-        self.messages.append(msg)  # TODO: Remove this
         self.query_one("#input").value = ""
 
-        # This updates our view for our messages - need to add view update for
-        # recieved messages too
-        view = self.query_one("#history", Static)
-        view.update("\n".join([i for i in self.messages]))
-
         msg_to_send = f"{self.user}: {msg}"
-        self.sock.send(msg_to_send.encode("utf-8"))
+        log("MSG_TO_SEND: " + msg_to_send)
+        self.send_sock.send(msg_to_send.encode("utf-8"))
